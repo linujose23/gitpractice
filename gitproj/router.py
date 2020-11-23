336f97f5dd17a67332ca85eb661265217051ff4a
linujose23 <linujose23@gmail.com>
2020-11-23 21:32:45 +0530

    flask change 2

---
+++ gitproj/router.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+
+# vcvcx
+=======
+>>>>>>> 96a0994... added flask code
+=======
+>>>>>>> 96a0994... added flask code
